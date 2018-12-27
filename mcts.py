b7d9bf0c756584cdd20622360e1b60553ee64c6a
Jerome Wei <jeromew@berkeley.edu>
2018-12-27 14:10:42 -0800

    did some things

--- mcts.py
+++ mcts.py
@@ -1,7 +1,17 @@
 from spades import *
 
 def children(player, hand, table, bids, history, spades_broken):
-    #Build state
+    #Build state from information
+    #Use probability to create state
+    hands = []
+    probable_state = GameState.from_(
+        bids,
+        hands,
+        table,
+        player,
+        history,
+        spades_broken
+    )
 
 class Node:
     def __init__(self):

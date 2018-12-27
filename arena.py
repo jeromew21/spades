87978ce8736e01e315f5a78d1a79fbef96684b09
Jerome Wei <jeromew@berkeley.edu>
2018-12-27 12:23:31 -0800

    er

--- arena.py
+++ arena.py
@@ -32,7 +32,11 @@ def match():
     record = [0, 0]
     errors = 0
     for i in range(100):
-        winner = fight(jerome_ai, play_turn)
+        try:
+            winner = fight(jerome_ai, play_turn)
+        except:
+            winner = None
+            errors += 1
         if winner == jerome_ai:
             record[0] += 1
         elif winner == play_turn:
